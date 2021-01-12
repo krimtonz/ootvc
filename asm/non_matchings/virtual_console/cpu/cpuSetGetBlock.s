6913436e1e0b609b11165172c4feef8acc20003a
KrimtonZ <krimtonz@gmail.com>
2021-01-12 15:24:25 -0600

    split cpu

---
+++ asm/non_matchings/virtual_console/cpu/cpuSetGetBlock.s
@@ -0,0 +1,5 @@
+glabel cpuSetGetBlock
+/* 8003C7A4 00037D64  90 A4 00 2C */	stw r5, 0x2c(r4)
+/* 8003C7A8 00037D68  38 60 00 01 */	li r3, 1
+/* 8003C7AC 00037D6C  4E 80 00 20 */	blr 
+
---
+++ src/virtual_console/cpu.c
@@ -0,0 +1,107 @@
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuExecuteUpdate.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_DSLLV.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_DSRLV.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_DSRAV.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_DMULT.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_DMULTU.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_DDIV.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_DDIVU.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_S_SQRT.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_D_SQRT.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_W_CVT_SD.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_L_CVT_SD.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_CEIL_W.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_FLOOR_W.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_LB.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_LH.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_LW.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_LBU.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_LHU.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_SB.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_SH.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_SW.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_LDC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_SDC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_LWL.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuCompile_LWR.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuExecuteOpcode.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuExecuteIdle.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuExecuteJump.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuExecuteCall.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuExecute_LoadStore.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuExecuteLoadStoreF.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuMakeLink.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuExecute.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003C614.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003C708.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuSetGetBlock.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuSetDeviceGet.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuSetDevicePut.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003C7E0.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003C84C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003CF50.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003D004.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003D088.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003D344.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuGetAddressBuffer.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003D47C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003D5FC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003D67C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuHeapTake.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuHeapFree.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuTreeTake.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/cpuFindFunction.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/cpu/func_8003E604.s")
